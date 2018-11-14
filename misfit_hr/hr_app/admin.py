10d0a9ff0fe43722b82f99debebf631d8b837555
Suja Uddin <suja.uddin@ipay.com.bd>
2018-11-14 20:52:32 +0600

    Admin customization

--- misfit_hr/hr_app/admin.py
+++ misfit_hr/hr_app/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
+from django.contrib import auth
 
-# Register your models here.
-from .models import User, Request
+from .models import User
+
+admin.site.site_header = 'MISFIT Administration'
+admin.site.site_title = 'MISFIT Administration'
+admin.site.unregister(auth.models.Group)
+admin.site.unregister(auth.models.User)
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     exclude = ('is_logged_in',)
+
+
